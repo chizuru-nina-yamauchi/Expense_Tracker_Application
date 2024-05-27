973b773f3a19f584b33d854786391b4c66ae8e22
Chizuru <chizuru.yamauchi@dci-student.org>
2024-05-27 11:30:12 +0200

    add missing annotation 'Repository' in Repository classes

--- src/main/java/com/example/expense_tracker_application/repositroy/UserRepository.java
+++ src/main/java/com/example/expense_tracker_application/repositroy/UserRepository.java
@@ -2,7 +2,10 @@ package com.example.expense_tracker_application.repositroy;
 
 import com.example.expense_tracker_application.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
 }
